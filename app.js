0e54f25cb736a3b30e02bebf3ac1e5ae5a3b2ba2
Oleg Futurecat <vtoroysite1@yandex.ru>
2021-06-11 16:56:58 +0300

    Сделать проект 14 фикс

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ app.use('/', pageNotFoundRouter);
 
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
-  const { statusCode = 500, message } = 'На сервере произошла ошибка';
+  const { statusCode = 500, message } = err;
   res
     .status(statusCode)
     .send({
       // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === 500
-        ? 'ОДНА АШИПКА И ТЫ АШИПСЯ'
+        ? 'На сервере произошла ошибка'
         : message,
     });
   next();
